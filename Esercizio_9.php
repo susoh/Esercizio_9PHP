d2671acfff55588d02e173936d6a90ac3c8bfd35
Davide Susini <susini.davide@itismeucci.com>
2024-09-27 09:42:33 +0200

    esercizio modificato

--- Esercizio_9.php
+++ Esercizio_9.php
@@ -7,9 +7,11 @@
 </head>
 <body>
     <?php
-        $colore = "brown";
-        $nome = "Gabriel";
-        echo "<h1 style='color: $colore'> $nome </h1>"
+        $red = rand(0,255);
+        $green = rand(0,255);
+        $blue = rand(0,255);
+        $nome = "Marco";
+        echo "<h1 style='color: rgb($red,$green,$blue)'> $nome </h1>"
     ?>
 </body>
 </html>
